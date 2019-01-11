19b7b446fda044d1d3434ee3fd36586c43adb6ac
MAHY-MAHY <45997096+MAHY-MAHY@users.noreply.github.com>
2019-01-11 14:00:56 +0100

    Update egein.cpp

--- egein.cpp
+++ egein.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
+
 #include "egein.hpp"
 
-Laplacien(){
-  m_a=0;
-  m_b=1;
-  m_N=2;
-  m_h=(m_b-m_a)/(m_N+1);
+Laplacien::Laplacien(): m_a(0.), m_b(1.), m_N(2), m_h((m_b-m_a)/(m_N+1))
+{
 }
 
-Laplacien(const double a, const double b, const int N ){
-  m_a=a;
-  m_b=b;
-  m_N=N;
-  m_h=(b-a)/(N+1);
+Laplacien::Laplacien(const double a, const double b, const int N ): m_a(a), m_b(b), m_N(N), m_h( (m_b-m_a)/(m_N+1))
+{
 }
 
-void Matlaplacien(){}
+double Laplacien::f(double x) const
+{
+	return -std::exp(-x) * (2. + 2.*x - m_a - m_b - (x-m_a)*(x-m_b));
+}
 
-void TermeSource(){}
+void Laplacien::Matlaplacien(){}
 
-void SolveurDirect(){}
+void Laplacien::TermeSource(){}
 
-double CalcErreur(){}
+void Laplacien::SolveurDirect(){}
 
-void Save(std::string fichier){}
+double Laplacien::CalcErreur(){}
+
+void Laplacien::Save(std::string fichier){}
